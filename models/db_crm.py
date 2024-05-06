15f85493b9d0974fab0fac31c6f8c33adaa1d9b2
Jordan Wooley <jordanwooley95@gmail.com>
2024-05-06 15:03:22 -0400

    setup tables

---
+++ models/db_crm.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+db.define_table(
+    "companies",
+    Field("company_name", notnull=True, format="%(company_name)s"),
+    Field("address", notnull=True),
+    Field("industry", notnull=True),
+    Field("socials"),
+    Field("phone_number"),
+    format="%(company_name)s"
+)
+
+db.define_table(
+    "persons",
+    Field("person_name", notnull=True),
+    Field("company_name", "reference companies", notnull=True),
+    Field("title"),
+    Field("work_phone", requires=IS_MATCH("[\d\-\(\) ]+")),
+    Field("cell_phone", requires=IS_MATCH("[\d\-\(\) ]+")),
+    Field("email", requires=IS_EMAIL()),
+    Field("birthday", requires=IS_DATE(format=T("%Y-%m-%d"))),
+    Field("company_type", requires=IS_IN_SET(["Customer", "Vendor"])),
+    Field("comm_type", requires=IS_IN_SET(["Web", "Phone", "Email", "In person"])),
+    Field("comments", "text"),
+    Field("created_by", "reference auth_user", default=auth.user_id),
+    Field("created_on", "datetime", default=request.now),
+    format="%(person_name)s"
+)
+
+db.define_table(
+    "events",
+    Field("user_name", "reference auth_user"),
+    Field("person_name", "reference persons"),
+    Field("comm_type", requires=IS_IN_SET(["Phone", "Email", "In person"])),
+    Field("event_date", requires=IS_DATETIME()),
+    Field("event_time", requires=IS_TIME()),
+    Field("status", requires=IS_IN_SET(["Open", "Closed", "Canceled"])),
+    Field("event_type", requires=IS_IN_SET(["Meeting", "Call", "Email", "Lunch", "Dinner", "Breakfast", "Other"])),
+    Field("comments", "text"),
+    format="%(event_type)s"
+)
