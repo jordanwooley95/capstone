2fc073ea0e3bb59e50a215fcfaca5439db02cdca
Jordan Wooley <47928010+jordanwooley95@users.noreply.github.com>
2024-05-07 22:32:42 -0400

    fixed more tables

--- models/db_crm.py
+++ models/db_crm.py
@@ -5,6 +5,7 @@ db.define_table(
     Field(
         "name_of_state",
         notnull=True,
+        unique=True,
         requires=IS_IN_SET(
             sorted(
                 [
@@ -43,10 +44,9 @@ db.define_table(
 
 db.define_table(
     "companies",
-    Field("company_id", notnull=True),
     Field("company_name", notnull=True),
     Field("address", notnull=True),
-    Field("state_id", "reference states", notnull=True),
+    Field("name_of_state", "reference states", notnull=True),
     Field("industry", notnull=True),
     Field("website", requires=IS_URL()),
     Field("linkedin"),
@@ -56,7 +56,6 @@ db.define_table(
 
 db.define_table(
     "persons",
-    Field("person_id", notnull=True),
     Field("company_id", "reference companies", notnull=True),
     Field("first_name", notnull=True),
     Field("last_name", notnull=True),
@@ -70,17 +69,22 @@ db.define_table(
     Field("comments", "text"),
     Field("created_by", "reference auth_user", default=auth.user_id),
     Field("created_on", "datetime", default=request.now),
-    format="%(person_id)s",
+    format="%(last_name)s, %(first_name)s",
 )
 
 db.define_table(
     "events",
     Field("user_id", "reference auth_user"),
-    Field("person_id", "reference persons"),
+    Field("person_id","reference persons",),
     Field("comm_type", requires=IS_IN_SET(["Phone", "Email", "In person"])),
     Field("event_datetime", "datetime", requires=IS_DATETIME()),
     Field("status", requires=IS_IN_SET(["Open", "Closed", "Canceled"])),
-    Field("event_type", requires=IS_IN_SET(["Meeting", "Call", "Email", "Lunch", "Dinner", "Breakfast", "Other"])),
+    Field(
+        "event_type",
+        requires=IS_IN_SET(
+            ["Meeting", "Call", "Email", "Lunch", "Dinner", "Breakfast", "Other"]
+        ),
+    ),
     Field("comments", "text"),
     format="%(event_type)s",
 )
