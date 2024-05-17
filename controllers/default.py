09b560e63ecc3bab07dd5146a434e517188768fb
Jordan Wooley <47928010+jordanwooley95@users.noreply.github.com>
2024-05-16 20:53:00 -0400

    format

--- controllers/default.py
+++ controllers/default.py
@@ -4,73 +4,92 @@
 # this file is released under public domain and you can use without limitations
 # -------------------------------------------------------------------------
 
+
 # ---- example index page ----
 def index():
-    return dict(message=T('Welcome to Joint Ventures!'))
+    return dict(message=T("Welcome to Joint Ventures!"))
+
 
 def about():
     return dict(message="About us")
 
+
 def productz():
     return dict(message="Our products")
+
 
 @auth.requires_login()
 def cannalytics():
     return dict(message="Hello from Cannalytics!")
 
+
 @auth.requires_login()
 def dataadmin():
     return dict(message="Hello from Cannalytics!")
+
 
 @auth.requires_login()
 def brands():
     grid = SQLFORM.grid(db.brands)
     return dict(grid=grid)
 
+
 @auth.requires_login()
 def customers():
     grid = SQLFORM.grid(db.customers)
     return dict(grid=grid)
+
 
 @auth.requires_login()
 def events():
     grid = SQLFORM.grid(db.events)
     return dict(grid=grid)
 
+
 @auth.requires_login()
 def states():
     grid = SQLFORM.grid(db.states)
     return dict(grid=grid)
+
 
 @auth.requires_login()
 def products():
     grid = SQLFORM.grid(db.products)
     return dict(grid=grid)
 
+
 @auth.requires_login()
 def orders():
     grid = SQLFORM.grid(db.orders)
     return dict(grid=grid)
 
+
 # ---- API (example) -----
 @auth.requires_login()
 def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
+    if not request.env.request_method == "GET":
+        raise HTTP(403)
+    return response.json({"status": "success", "email": auth.user.email})
+
 
 # ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+@auth.requires_membership("admin")  # can only be accessed by members of admin groupd
 def grid():
-    response.view = 'generic.html' # use a generic view
+    response.view = "generic.html"  # use a generic view
     tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    if not tablename in db.tables:
+        raise HTTP(403)
+    grid = SQLFORM.smartgrid(
+        db[tablename], args=[tablename], deletable=False, editable=False
+    )
     return dict(grid=grid)
+
 
 # ---- Embedded wiki (example) ----
 def wiki():
-    auth.wikimenu() # add the wiki to the menu
-    return auth.wiki() 
+    auth.wikimenu()  # add the wiki to the menu
+    return auth.wiki()
+
 
 # ---- Action for login/register/etc (required for auth) -----
 def user():
@@ -90,6 +109,7 @@ def user():
     also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
     """
     return dict(form=auth())
+
 
 # ---- action to server uploaded static content (required) ---
 @cache.action()
