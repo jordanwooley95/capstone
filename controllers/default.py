5a049119d5f5f2395275d70e7120b0968fef9858
ccolkereybel <163072600+ccolkereybel@users.noreply.github.com>
2024-05-28 19:18:54 -0400

    trying to fix dates

--- controllers/default.py
+++ controllers/default.py
@@ -35,7 +35,7 @@ def index():
         # Calculate days ago
         current_datetime = datetime.datetime.now()
         order_datetime = order.orders.ordered_on
-        days_ago = abs((current_datetime - order_datetime)).days
+        days_ago = (current_datetime - order_datetime).days
 
         # Prepare the order details
         order_details = {
